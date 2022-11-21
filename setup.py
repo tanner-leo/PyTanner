685f41d2d139b3f58149a1242689c4edf7293513
tanmann13 <tanner.m.leo@gmail.com>
2022-11-21 15:55:52 -0800

    Changes!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r",encoding="utf-8") as fh:
 
 setuptools.setup(
     name='PyTanner',
-    version='0.0.4',
+    version='0.0.5',
     author='Tanner Leo',
     description='Various Electrochemistry Related Functions',
     long_description = long_description,
