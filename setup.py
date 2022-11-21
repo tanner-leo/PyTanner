4fbe9db4c141057e898196aa4a12778a5a5879f3
tanmann13 <tanner.m.leo@gmail.com>
2022-11-21 15:27:50 -0800

    Try 3

---
+++ setup.py
@@ -0,0 +1,16 @@
+import PyTanner
+
+with open("README.md","r",encoding="utf-8") as fh:
+    long_description=fh.read()
+
+PyTanner.setup(
+    name='PyTanner',
+    version='0.0.2',
+    author='Tanner Leo',
+    description='Various Electrochemistry Related Functions',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    packages=['PyTanner'],
+    install_requires=['pandas','matplotlib','numpy'],
+    url='https://github.com/tanmann13/EchemTools.git'
+)
