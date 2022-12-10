c49b90d591842b52d711c946657598a7371b9d1e
tanmann13 <tanner.m.leo@gmail.com>
2022-12-10 10:34:36 -0800

    Tesing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r",encoding="utf-8") as fh:
 
 setuptools.setup(
     name='PyTanner',
-    version='0.1.0',
+    version='0.1.1',
     author='Tanner Leo',
     description='Various Electrochemistry Related Functions',
     long_description = long_description,
